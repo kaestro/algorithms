a5bc611f9c088ee6975c8f1fcb1f16088c8c200a
kimtaehong <didme07@gmail.com>
2019-10-06 14:25:04 +0900

    Easy leetcode problems that doesn't seems reviews needed

---
+++ leetcode/easy/5213.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minCostToMoveChips(self, chips: List[int]) -> int:
+        odd, even = 0, 0
+        for chip in chips:
+            if chip % 2 == 0:
+                even += 1
+            else:
+                odd += 1
+        return min(even, odd)
