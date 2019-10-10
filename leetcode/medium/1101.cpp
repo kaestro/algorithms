34a99f71ee2360483701b7fa513674e14789f5b5
kimtaehong <didme07@gmail.com>
2019-10-11 07:39:31 +0900

    UnionFind Problem.
     Find Parent and compress path(line 6:11)
    
     Find if all people are friends(line 19:28)
    
     input needs to be sorted by time.(line 39)
    
     At least N - 1 unions should be repeated, for all elements to be
     united.(line 38)

---
+++ leetcode/medium/1101.cpp
@@ -0,0 +1,68 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int findParent(vector<int>& parent, int index) {
+    if (parent[index] != index) {
+        parent[index] = findParent(parent, parent[index]);
+    }
+    return parent[index];
+}
+
+void unionChild(vector<int>& parent, int x, int y) {
+    int xroot = findParent(parent, x);
+    int yroot = findParent(parent, y);
+    parent[yroot] = xroot;
+}
+
+bool allFriends(vector<int>& parent) {
+    int N = parent.size();
+    int root = findParent(parent, 0);
+    for (int i = 1; i < N; ++i) {
+        if (findParent(parent, i) != root) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool compare(vector<int>& left, vector<int>& right) {
+    return left[0] < right[0];
+}
+
+class Solution {
+public:
+    int earliestAcq(vector<vector<int>>& logs, int N) {
+        int size = logs.size();
+        if (size < N) return -1;
+        sort(logs.begin(), logs.end(), compare);
+
+        vector< int > parent(N);
+        for (int i = 0; i < N; ++i)
+            parent[i] = i;
+
+        int time_max = -1;
+        for (int i = 0; i < N - 1; ++i) {
+            unionChild(parent, logs[i][1], logs[i][2]);
+            time_max = max(time_max, logs[i][0]);
+        }
+
+        for (int i = N - 1; i < size; ++i) {
+            unionChild(parent, logs[i][1], logs[i][2]);
+            time_max = max(time_max, logs[i][0]);
+            if (allFriends(parent))
+                return time_max;
+        }
+
+        return -1;
+    }
+};
+
+int main() {
+    vector<vector<int>> logs = {
+        {20190101, 0, 1}, {20190107, 2, 3}, {20190211, 1, 5}, {20190224,2,4}, {20190301,0,3}, {20190312,1,2}, {20190322,4,5}
+    };
+    Solution s;
+    s.earliestAcq(logs, 6);
+}
