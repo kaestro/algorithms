e34cdceb2110abd799e39e4d9db7bd2bfb7e1336
kimtaehong <didme07@gmail.com>
2019-10-21 23:39:31 +0900

    Replaced returning result using join
     line 49

---
+++ leetcode/trie/explore/practical_application/replace_words3.py
@@ -0,0 +1,55 @@
+import collections
+from typing import List
+
+class TrieNode(object):
+    
+    def __init__(self):
+        self.children = collections.defaultdict(TrieNode)
+        self.isEnd = False
+
+class Trie(object):
+
+    def __init__(self):
+        self.root = TrieNode()
+    
+
+    def insert(self, input: str) -> None:
+        cur_node = self.root
+        for letter in input:
+            if cur_node.children.get(letter) == None:
+                cur_node.children[letter] = TrieNode()
+            cur_node = cur_node.children[letter]
+        cur_node.isEnd = True
+    
+
+    def findShortestRoot(self, input: str) -> str:
+        cur_node = self.root
+        result = ""
+        for letter in input:
+            if cur_node.children.get(letter) == None:
+                return input
+            else:
+                result += letter
+                cur_node = cur_node.children[letter]
+                if cur_node.isEnd:
+                    return result
+        if cur_node.isEnd:
+            return result
+        else:
+            return input
+
+
+class Solution:
+    def replaceWords(self, dict: List[str], sentence: str) -> str:
+        roots = Trie()
+        for root in dict:
+            roots.insert(root)
+        
+        sentence = sentence.split(" ")
+        return " ".join(map(roots.findShortestRoot, sentence))
+
+
+if __name__ == "__main__":
+    roots = ["cat", "bat", "rat"]
+    sentence = "the cattle was rattled by the battery"
+    print(Solution().replaceWords(roots, sentence))
