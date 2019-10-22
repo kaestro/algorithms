8701c32b588c057e38d232ab293fbe46949506e9
kimtaehong <didme07@gmail.com>
2019-10-22 09:34:53 +0900

    Passing version of string_compression
     You need to increment the cur_length whenever repeated_number's number
    of digits changes.
    (line 46:51)

--- programmers/kakao blind/2020/string_compression.cpp
+++ programmers/kakao blind/2020/string_compression.cpp
@@ -1,41 +1,57 @@
-#include <vector>
 #include <string>
+#include <vector>
 #include <iostream>
 
 using namespace std;
 
+// compress input with given unit length.
+// early termination when compressed string becomes longer than min_length and return min_length
+// min_length is the length of currently shortest compressed string
+int compressed_string_length_by_unit(const string& input, int unit_length, int min_length);
+
 int solution(string input) {
-    int str_length, min_length;
-    min_length = str_length = input.length();
-
-    for (auto unit_length = 1; unit_length <= str_length / 2; ++unit_length)
-    {
-        size_t str_idx = 0;
-        int compressed_length = str_length;
-        while (str_idx < str_length)
-        {
-            string cur_string = input.substr(str_idx, unit_length);
-            size_t next_idx = str_idx + unit_length;
-            int repeated_cnt = 0;
-            while (next_idx < str_length && cur_string == input.substr(next_idx, unit_length))
-            {
-                next_idx += unit_length;
-                repeated_cnt += 1;
-            }
-
-            if (repeated_cnt != 0)
-            {
-                compressed_length += 1;
-                compressed_length -= unit_length * repeated_cnt;
-                str_idx = next_idx;
-            } else {
-                str_idx += 1;
-            }
-        }
-        min_length = min(compressed_length, min_length);
+    int input_length = input.length();
+    int min_length = input_length;
+    for (auto unit_length = 1; unit_length <= input_length / 2; ++unit_length) {
+        int cur_length = compressed_string_length_by_unit(input, unit_length, min_length);
+        min_length = min(cur_length, min_length);
     }
 
     return min_length;
+}
+
+int compressed_string_length_by_unit(const string& input, int unit_length, int min_length)
+{
+    int cur_length = unit_length; // current length of compressed string
+    int input_length = input.length();
+    int num_unit_repeated = 1;
+
+    string cur_substr = input.substr(0, unit_length);
+    for (auto str_index = unit_length; cur_length < min_length && str_index < input_length; str_index += unit_length)
+    {
+        if (str_index + unit_length > input_length) 
+        {
+            cur_length += (input_length - str_index);
+            break;
+        }
+
+        string next_substr = input.substr(str_index, unit_length);
+        if (cur_substr != next_substr) 
+        {
+            cur_length += unit_length;
+            cur_substr = next_substr;
+            num_unit_repeated = 1;
+        } else {
+            num_unit_repeated += 1;
+            switch (num_unit_repeated)
+            {
+                case 2: cur_length++; break;
+                case 10: cur_length++; break;
+                case 100: cur_length++; break;
+            }
+        }
+    }
+    return cur_length;
 }
 
 int main() {
